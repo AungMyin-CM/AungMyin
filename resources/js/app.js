493fbc91d7e0546e786a9f3f7851001be065a0ac
Krishna <aryalkrishna642@gmail.com>
2022-02-23 23:45:33 +0630

    Add preloader and catch action on user input to hide validation's exception

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
-require('./bootstrap');
+import './bootstrap';
+
+function start_loader() {
+    $('body').append('<div class="preloader flex-column justify-content-center align-items-center"></div>')
+
+}
+
+function end_loader() {
+    $('.preloader').fadeOut('fast',function () {
+        $('.preloader').remove();
+    })
+}
+$("input").on('keyup change',function() {
+    $("#alert-message").remove();
+})
+
+window.onload = function () {
+    end_loader();
+  }
+
+start_loader();
+
