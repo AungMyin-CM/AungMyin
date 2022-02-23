3cea34ae2c673583a54280fe0edb1615a1608a29
Krishna <aryalkrishna642@gmail.com>
2022-02-23 23:49:36 +0630

    Retrive old post data on registration fail

--- resources/views/registration/clinic_registration.blade.php
+++ resources/views/registration/clinic_registration.blade.php
@@ -1,15 +1,28 @@
-@extends('layouts.app')
+@include('layouts.app')
 <body class="hold-transition register-page">
     <div class="register-box">
       <div class="register-logo">
-        <a href="../../index2.html"><b>Input </b>Your Clinic Name</a>
+        <a href="../../index2.html"><b>Input </b><span id="clinicName"></span></a>
       </div>
       <div class="card">
         <div class="card-body register-card-body">
-    
-          <form action="{{ route('register-clinic') }}" method="GET">
+          @if ($errors->any())
+            <div class="alert alert-danger">
+              <ul>
+                  @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                  @endforeach
+              </ul>
+            </div><br />
+          @endif
+          <form action="{{ route('clinic.register') }}" method="POST">
+
+            @csrf
+
             <div class="input-group mb-3">
-              <input type="text" class="form-control" name="clinic_code" placeholder="Code">
+              <input type="text" class="form-control" name="code" placeholder="Code" value="{{ old('code') }}">
+              <input type="hidden" class="form-control" name="package_id" id="package_id" value="1">
+              <input type="hidden" class="form-control" name="name" id="clinic_input_name">
               <div class="input-group-append">
                 <div class="input-group-text">
                   <span class="fas fa-clinic-medical"></span>
@@ -18,7 +31,7 @@
             </div>
             
             <div class="input-group mb-3">
-                <input type="email" class="form-control" name="clinic_email" placeholder="Email">
+                <input type="email" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}">
                 <div class="input-group-append">
                   <div class="input-group-text">
                     <span class="fas fa-envelope"></span>
@@ -26,7 +39,7 @@
                 </div>
               </div>
               <div class="input-group mb-3">
-                <input type="password" class="form-control" name="password" placeholder="Password">
+                <input type="password" class="form-control" name="password" placeholder="Password" value="{{ old('password') }}">
                 <div class="input-group-append">
                   <div class="input-group-text">
                     <span class="fas fa-eye"></span>
@@ -34,7 +47,7 @@
                 </div>
               </div>
               <div class="input-group mb-3">
-                <input type="password" class="form-control" placeholder="Retype password">
+                <input type="password" class="form-control" name="password_confirmation" placeholder="Retype password">
                 <div class="input-group-append">
                   <div class="input-group-text">
                     <span class="fas fa-eye"></span>
@@ -42,7 +55,7 @@
                 </div>
               </div>
               <div class="input-group mb-3">
-                <input type="tel" class="form-control" placeholder="Phone Number">
+                <input type="tel" class="form-control" placeholder="Phone Number" name="phoneNumber" value={{ old('phoneNumber') }}>
                 <div class="input-group-append">
                   <div class="input-group-text">
                     <span class="fas fa-phone"></span>
@@ -50,7 +63,7 @@
                 </div>
               </div>
               <div class="input-group mb-3">
-                <textarea rows="5" class="form-control" placeholder="Address"></textarea>
+                <textarea rows="5" class="form-control" placeholder="Address" name="address" value="{{ old('address') }}"></textarea>
                 <div class="input-group-append">
                   
                 </div>
@@ -68,6 +81,8 @@
     </div>
     <!-- /.register-box -->
     @extends('layouts.js')
+    <script src="{{ asset('js/components/clinic-register.js') }}"></script>
+
     <!-- jQuery -->
     
     </body>
