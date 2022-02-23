dac250f7a4dfd3e5c9e529d73b7a641bd6c898aa
Krishna <aryalkrishna642@gmail.com>
2022-02-23 23:48:37 +0630

    Added radio button on package selection in clinic registration

--- resources/views/registration/clinic_name.blade.php
+++ resources/views/registration/clinic_name.blade.php
@@ -1,34 +1,66 @@
-@extends('layouts.app')
+@include('layouts.app')
 <body class="hold-transition register-page">
     <div class="register-box">
+      <div class="row">
+        <div class="col-sm-4">
+          <!-- radio -->
+          <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input" id="packageId" type="radio" value="1" name="package_id" checked>
+              <label class="form-check-label">Trial</label>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <!-- radio -->
+          <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input" id="packageId" type="radio" value="2" name="package_id">
+              <label class="form-check-label">Basic</label>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <!-- radio -->
+          <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input" id="packageId" type="radio" value="3" name="package_id">
+              <label class="form-check-label">Premium</label>
+            </div>
+          </div>
+        </div>  
+      </div>
       <div class="register-logo">
         <a href="../../index2.html"><b>Input </b>Your Clinic Name</a>
       </div>
+      
+      
       <div class="card">
         <div class="card-body register-card-body">
-    
-          <form action="{{ route('register-clinic') }}" method="GET">
             <div class="input-group mb-3">
-              <input type="text" class="form-control" name="clinic_name" placeholder="Name">
+              <input type="text" id="clinic_name" class="form-control" name="clinic_name" placeholder="Name">
               <div class="input-group-append">
                 <div class="input-group-text">
                   <span class="fas fa-clinic-medical"></span>
                 </div>
               </div>
             </div>
+            <span id="error_clinic_name" style="color:red"></span>
+
 
               <!-- /.col -->
               <div class="col-4 float-right">
-                <button type="submit" class="btn btn-primary btn-block">Continue</button>
+                <input type="submit" id="register_clinic" class="btn btn-primary btn-block" value="Continue" />
               </div>
               <!-- /.col -->
-          </form>
         </div>
         <!-- /.form-box -->
       </div><!-- /.card -->
     </div>
     <!-- /.register-box -->
     @extends('layouts.js')
+    <script src="{{ asset('js/components/clinic-name.js') }}"></script>
     <!-- jQuery -->
     
     </body>
+    
