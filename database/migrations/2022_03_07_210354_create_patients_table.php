9fb6ddf2e5e5428cc8faaa46c623bbb3600e11fd
Krishna <aryalkrishna642@gmail.com>
2022-03-12 23:49:10 +0630

    Add new migration file for patient module

---
+++ database/migrations/2022_03_07_210354_create_patients_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+            $table->string('clinic_code');
+            $table->string('name');
+            $table->string('father_name')->nullable();
+            $table->integer('age');
+            $table->bigInteger('phoneNumber')->nullable();
+            $table->longText('address');
+            $table->tinyInteger('gender');
+            $table->longText('summary')->nullable();
+            $table->longText('drug_allergy')->nullable();
+            $table->integer('status')->default(1);
+            $table->integer('is_followup')->default(0);
+            $table->timestamp('followup_date')->nullable();
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
