2f20d37e92498160d047fbc64479eaa8a20e16f7
Krishna <aryalkrishna642@gmail.com>
2022-03-12 23:40:24 +0630

    Add new routes with auth middleware wrapped for user and clinic

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClinicController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DictionaryController;
+use App\Http\Controllers\PatientController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,31 +19,60 @@ use App\Http\Controllers\LoginController;
 |
 */
 
-Route::get('/', function () {
-    return view('login.login');
-})->name('login.login');
+Route::get('/', [LoginController::class, 'index'])->name('user-login')->middleware('guest');
 
 Route::get('clinic-name', function () {
-    return view('registration.clinic_name');
+    return view('registration.clinic_name');    
 })->name('clinic-name');
 
 Route::get('register-clinic', function () {
     return view('registration.clinic_registration');
 })->name('register-clinic');
 
-Route::get('clinic-home', function () {
-    return view('clinic.dashboard');
-})->name('clinic.home');
-
-
-Route::get('user/create',[ClinicController::class,'newUser'])->name('user.create');
-
-Route::get('users',[ClinicController::class,'index'])->name('user.list');
+Route::get('clinic-login', function () {
+    return view('login.clinic');
+})->name('login-clinic');
 
 Route::post('register-clinic',[ClinicController::class, 'register'])->name('clinic.register');
 
-Route::post('register-user',[ClinicController::class, 'registerUser'])->name('user.register');
+Route::group(['middleware' => 'auth'], function () {
 
-Route::get('logout', [ClinicController::class, 'logout'])->name('logout');
+    Route::resource('dictionary',DictionaryController::class);
 
-Route::post('login',[LoginController::class, 'login'])->name('login');
+    Route::get('home', function () {
+        return view('clinic.home');
+    })->name('user.home');
+
+    Route::post('userlogout', [LoginController::class, 'userLogout'])->name('user.logout');
+
+    Route::resource('patient',PatientController::class);
+
+    Route::post('/fetchDictionary',[PatientController::class, 'fetchDictionary'])->name('dictionary.get');
+
+});
+
+
+Route::post('logout', [LoginController::class, 'clinicLogout'])->name('clinic.logout');
+
+Route::group(['middleware' => 'prevent-back-history'],function(){
+    
+    Route::post('clinic-login',[LoginController::class, 'clinicLogin'])->name('clinic.login');
+
+    Route::post('user-login',[LoginController::class, 'userLogin'])->name('user.login');
+
+});
+
+Route::group(['middleware' => 'clinic.auth'], function() {
+
+    Route::get('users',[ClinicController::class,'index'])->name('user.list');
+
+    Route::get('user/create',[ClinicController::class,'newUser'])->name('user.create');
+
+    Route::post('register-user',[ClinicController::class, 'registerUser'])->name('user.register');
+
+    Route::get('dashboard', function () {
+        return view('clinic.dashboard');
+    })->name('clinic.home');
+
+
+});
