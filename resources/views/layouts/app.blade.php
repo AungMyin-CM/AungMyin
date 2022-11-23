769746f2ab2af938e3d96b65c1a6ae16b142c3cc
santoshgtm95 <santosh.gtm6@gmail.com>
2022-11-23 16:17:04 +0630

    [commit] pusher impl

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,8 +36,34 @@
     <link rel="stylesheet" href="{{ asset('plugins/jquery-ui/jquery-ui.css') }}">
 
     <link rel="stylesheet" href="{{ asset('css/selectize.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.css" integrity="sha512-DIW4FkYTOxjCqRt7oS9BFO+nVOwDL4bzukDyDtMO7crjUZhwpyrWBFroq+IqRe6VnJkTpRAS6nhDvf0w+wHmxg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/js/iziToast.min.js" integrity="sha512-Zq9o+E00xhhR/7vJ49mxFNJ0KQw1E1TMWkPTxrWcnpfEFDEXgUiwJHIKit93EW/XxE31HSI5GEOW06G6BF1AtA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+    <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+    <script>
+      
+      // Enable pusher logging - don't include this in production
+      Pusher.logToConsole = true;
+    
+      var pusher = new Pusher('4477b1080cfccdc685cf', {
+        cluster: 'ap1'
+      });
+      var clinicId ={{ Session::get('cc_id') }};
+      var userId = "<?php echo  Auth::guard('user')->user() ? Auth::guard('user')->user()['id'] : '0' ?>";
+      var channel = pusher.subscribe('aungmyin_'+ clinicId + "_" + userId);
+      channel.bind('notice', function(data) {
+       
+        iziToast.show({
+            position: 'topRight',
+            titleColor: '#FFFFFF',
+            messageColor: '#FFFFFF',
+            timeout: 20000,
+            backgroundColor: '#95bb72',        
+            message: data.message
+            });
+      });
+    </script>
 </head>
 <div class="middle">
     <div class="bar bar1"></div>
