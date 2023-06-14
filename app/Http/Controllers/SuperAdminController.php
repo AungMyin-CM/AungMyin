322943163393d1585b3ad24d0e2023c9ce801229
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2023-06-14 23:20:19 +0630

    feat: superadmin home page

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SuperAdminLoginRequest;
 use App\Models\User;
+use App\Models\Clinic;
+use App\Models\Package;
+use App\Models\PackagePurchase;
+use App\Models\UserClinic;
+use App\Http\Requests\SuperAdminLoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SuperAdminController extends Controller
 {
@@ -20,7 +25,7 @@ class SuperAdminController extends Controller
 
             $formFields = $request->only('code', 'password');
 
-            if (auth()->attempt($formFields) && $data->is_superadmin == true) {
+            if (Auth::guard('user')->attempt($formFields) && $data->is_superadmin == true) {
                 $request->session()->regenerate();
 
                 return redirect('aung_myin/admin_dashboard')->with('message', 'You are now logged in!');
@@ -32,13 +37,40 @@ class SuperAdminController extends Controller
 
     public function index()
     {
-        return view('superadmin.index');
+        // Get Total Users
+        $total_users = count(User::all());
+
+        // Get Package Purchased Users
+        $p_users = count(PackagePurchase::all());
+
+        // Get Unverified Users
+        $u_users = count(User::where('email_verified', 0)->get());
+
+        // Get Clinic Users
+        $c_users = count(UserClinic::all());
+
+        // Get Only Verified
+        $v_users = $total_users - ($c_users + $u_users);
+
+        // Get Total Clinics
+        $total_clinics = count(Clinic::all());
+
+        // Get Total Packages
+        $total_packages = count(Package::all());
+
+        return view('superadmin.index')
+            ->with('total_users', $total_users)
+            ->with('p_users', $p_users)
+            ->with('v_users', $v_users)
+            ->with('u_users', $u_users)
+            ->with('c_users', $c_users)
+            ->with('total_clinics', $total_clinics)
+            ->with('total_packages', $total_packages);
     }
 
     public function logout(Request $request)
     {
-        auth()->logout();
-
+        Auth::guard('user')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
