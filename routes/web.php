1885a26d08932ea62ed4e032128e9fd61acfa71c
Aung Naing Thu <aryalkrishna642@gmail.com>
2023-07-11 16:00:11 +0630

    Merge conflict solved

--- routes/web.php
+++ routes/web.php
@@ -186,6 +186,7 @@ Route::prefix('aung_myin/admin_dashboard')->group(function () {
     Route::post('/login', [SuperAdminController::class, 'authenticate'])->name('superadmin.login');
 
     Route::middleware(['superAuth'])->group(function () {
+<<<<<<< HEAD
         // Home route
         Route::get('/', [SuperAdminController::class, 'index'])->name('superadmin.index');
 
@@ -215,6 +216,13 @@ Route::prefix('aung_myin/admin_dashboard')->group(function () {
         Route::patch('/profile/{id}', [SuperAdminController::class, 'profileUpdate'])->name('superadmin.profileUpdate');
 
         // Logout
+=======
+        Route::get('/', [SuperAdminController::class, 'index'])->name('superadmin.index');
+        Route::get('/users', [SuperAdminController::class, 'users'])->name('superadmin.users');
+        Route::get('/users?type={type}', [SuperAdminController::class, 'users'])->name('superadmin.filter');
+        Route::get('/users/{id}', [SuperAdminController::class, 'edit'])->name('superadmin.edit');
+
+>>>>>>> e006e94 (feat: superadmin users page)
         Route::post('/logout', [SuperAdminController::class, 'logout'])->name('superadmin.logout');
     });
 });
