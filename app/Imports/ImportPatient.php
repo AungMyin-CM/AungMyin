46ecd148a74133ca1fc838359927e3eba1cf0134
Aung Naing Thu <aryalkrishna642@gmail.com>
2023-08-31 20:23:35 +0630

    Patient Excel import #WIP

--- app/Imports/ImportPatient.php
+++ app/Imports/ImportPatient.php
@@ -5,6 +5,8 @@ namespace App\Imports;
 use App\Models\Patient;
 use Maatwebsite\Excel\Concerns\ToModel;
 
+use Auth;
+
 class ImportPatient implements ToModel
 {
     /**
@@ -14,8 +16,33 @@ class ImportPatient implements ToModel
     */
     public function model(array $row)
     {
+        $user_id = Auth::guard('user')->user()['id'];
+
+        if      ($row["gender"] == "male")     $row["gender"] = 1 ;
+        elseif  ($row["gender"] == "female")     $row["gender"] = 0 ;
+
         return new Patient([
-            //
+
+            "name"              => $row['name'] ,
+            "father_name"       => $row["father_name"],
+            "age"               => $row["age"],
+            "phone_number"      => $row["phone_number"],
+            "address"           => $row["address"] ,
+            "gender"            => $row["gender"] ,
+            "summary"           => $row['summary'],
+            "drug_allergy"      => $row['drug_allergy']
+            
+
         ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'      => 'required',
+            'age'          => 'required',
+            'address'      => 'required',
+            'gender'       => 'required'
+        ];
     }
 }
