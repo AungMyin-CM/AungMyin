2a5bec828fbde0ace57e53f17044bf382d642b5e
kyawzinhtet <itskyawzinhtet@gmail.com>
2023-09-22 13:11:54 +0630

    chore: update visit record design

--- resources/views/patient/show.blade.php
+++ resources/views/patient/show.blade.php
@@ -1,6 +1,20 @@
 @extends("layouts.app")
 @section('content')
 
+<style>
+    .calendar-card {
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+
+    .calendar-day {
+        font-size: 32px;
+        font-weight: bold;
+    }
+</style>
+
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
         <div class="content-wrapper" style="background-color: {{config('app.bg_color')}} !important">
@@ -12,9 +26,9 @@
             </section>
 
             <section class="content">
-                <div class="container-fluid">                    
+                <div class="container-fluid">
                     <h5>Patient Information</h5>
-                    <div class="card">
+                    <div class="card mb-4">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-sm-3">
@@ -26,60 +40,80 @@
                                     <h6 id="drug_allergy"><b>Drug Allergy :</b> {{ $patient->drug_allergy ? $patient->drug_allergy : 'None'  }} </h6>
                                 </div>
                                 @if(isset($patient->disease[0]))
-                                    <div class="col-sm-3">
-                                        <h6 id="p_di"><b>Disease :</b> <span id="p_disease">{{ $patient->disease[0]->disease }}</span></h6>
-                                    </div>
+                                <div class="col-sm-3">
+                                    <h6 id="p_di"><b>Disease :</b> <span id="p_disease">{{ $patient->disease[0]->disease }}</span></h6>
+                                </div>
                                 @endif
                                 <div class="col-sm-3">
                                     <h6 id="p_di"><b>Father Name :</b> <span id="p_disease">{{ $patient->father_name }}</span></h6>
-                                </div>                            
+                                </div>
                             </div>
                         </div>
                     </div>
-                    
+
                     @if($visits->isEmpty())
-                        <h6 class="text-center text-red pt-5">No visits data found for this patient.</h6>
+                    <h6 class="text-center text-red pt-5">No visits data found for this patient.</h6>
                     @else
-                        <h5>Visits Record</h5>
-                        <div class="card">
-                            <div class="card-body">
-                                @foreach($visits as $visit)
-                                <div class="row mb-3 p-2 rounded" style="background-color: #FDFAF0;">
-                                    <div class="col-sm-4">
-                                        <p>
-                                            <span class="text-bold">Visit Date: </span><br>
-                                            {{ $visit->updated_at->format('d-M-Y g:iA') }}
-                                        </p>
+
+                    <h5>Visit Records</h5>
+                    <div class="row">
+                        @foreach($visits as $visit)
+                        <div class="col-lg-3 col-md-4 col-sm-6 mb-2">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <div class="calendar-card w-75">
+                                        <div class="calendar-day">
+                                            {{ $visit->updated_at->format('d') }}
+                                        </div>
+                                        <div class="calendar-month-year">
+                                            {{ $visit->updated_at->format('M Y') }}
+                                        </div>
                                     </div>
-                                    <div class="col-sm-4">
-                                        <p>
-                                            <span class="text-bold">Assigned Med: </span><br>
-                                            {!! $visit->assigned_medicines !!}
-                                        </p>
-                                    </div>
+
+                                    @php
+                                    $medRows = explode('<br>', $visit->assigned_medicines);
+
+                                    foreach ($medRows as $medRow) {
+                                        $parts = explode('^', $medRow);
+
+                                        foreach ($parts as $index => $part) {
+                                            if (!empty($part)) {
+                                                if ($index % 3 == 0) {
+                                                    $badgeClass = 'badge badge-primary';
+                                                } elseif ($index % 3 == 1) {
+                                                    $badgeClass = 'badge badge-secondary';
+                                                } else {
+                                                    $badgeClass = 'badge badge-info';
+                                                }
+
+                                                echo '<span class="' . $badgeClass . ' mt-3">' . $part . '</span> ';
+                                            }
+                                        }
+                                        if (!empty(array_filter($parts))) {
+                                            echo '<br>';
+                                        }
+                                    }
+                                    @endphp
+
                                     @if($visit->followup_date)
-                                    <div class="col-sm-4">
-                                        <p>
-                                            <span class="text-bold">Followup: </span><br>
-                                            {{ \Carbon\Carbon::parse($visit->followup_date)->diffForHumans() }}
-                                        </p>
-                                    </div>
+                                    <p class="mt-3">
+                                        <i class="fas fa-calendar-alt"></i>&nbsp;
+                                        {{ \Carbon\Carbon::parse($visit->followup_date)->diffForHumans() }}
+                                    </p>
                                     @endif
                                 </div>
-                                @endforeach
                             </div>
-                        </div>                        
+                        </div>
+                        @endforeach
+                    </div>
                     @endif
-                    
+
                 </div>
             </section>
         </div>
     </div>
 </body>
-<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('plugins/jquery-ui/jquery-ui.js') }}"></script>
 
-<script>
-   
-</script>
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<script></script>
 @endsection
